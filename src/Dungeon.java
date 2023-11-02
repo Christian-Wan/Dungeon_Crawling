cbc8b75397371c2871222ff5e1b5b397d50320b5
Christian Wan <christian.wan295@gmail.com>
2023-11-02 14:03:50 -0400

    Made it so that player can move through the map. Made outline of the Player class. Should make a class for each type of enemy.

--- src/Dungeon.java
+++ src/Dungeon.java
@@ -1,10 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Dungeon {
 
+    int playerRow = 4;
+    int playerColumn = 4;
     int[][] dungeonMap = new int[6][6];
     public Dungeon(String chooser) {
         File f = new File("maps/map" + chooser);
@@ -49,17 +52,73 @@ public class Dungeon {
 
     public boolean moveNorth() {
         boolean check = true;
-        if (dungeonMap[][])
-        return false;
+        if (dungeonMap[playerRow - 1][playerColumn] == 0) {
+            check = false;
+        }
+        return check;
     }
     public boolean moveEast() {
-        return false;
+        boolean check = true;
+        if (dungeonMap[playerRow][playerColumn + 1] == 0) {
+            check = false;
+        }
+        return check;
     }
     public boolean moveSouth() {
-        return false;
+        boolean check = true;
+        if (dungeonMap[playerRow + 1][playerColumn] == 0) {
+            check = false;
+        }
+        return check;
     }
     public boolean moveWest() {
-        return false;
+        boolean check = true;
+        if (dungeonMap[playerRow][playerColumn - 1] == 0) {
+            check = false;
+        }
+        return check;
+    }
+
+    public String movePlayer(String movement) {
+
+        if (movement.equals("n") && moveNorth()) {
+            dungeonMap[playerRow][playerColumn] = 5;
+            playerRow--;
+            return "You moved north";
+        }
+        else if (movement.equals("e") && moveEast()) {
+            dungeonMap[playerRow][playerColumn] = 5;
+            playerColumn++;
+            return "You moved east";
+        }
+        else if (movement.equals("s") && moveSouth()) {
+            dungeonMap[playerRow][playerColumn] = 5;
+            playerRow++;
+            return "You moved south";
+        }
+        else if (movement.equals("w") && moveWest()) {
+            dungeonMap[playerRow][playerColumn] = 5;
+            playerColumn--;
+            return "You moved west";
+        }
+
+        return "You walk into a wall";
+    }
+
+
+    public void runRoom() {
+        if (dungeonMap[playerRow][playerColumn] == 2) {
+            System.out.println("Bad");
+        }
+        else if (dungeonMap[playerRow][playerColumn] == 3) {
+            System.out.println("Good");
+        }
+        else if (dungeonMap[playerRow][playerColumn] == 4) {
+            System.out.println("Boss");
+        }
+        else {
+            System.out.println("Been");
+        }
     }
 
 
--- src/DungeonCrawlerRunner.java
+++ src/DungeonCrawlerRunner.java
@@ -1,4 +1,34 @@
 import java.util.Scanner;
 
 public class DungeonCrawlerRunner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String playerChoice;
+        Dungeon play;
+
+
+        System.out.print("Choose map (type 1-3 or anything else for random): ");
+        playerChoice = s.nextLine();
+
+        if (playerChoice.equals("1") || playerChoice.equals("2") || playerChoice.equals("3")) {
+            play = new Dungeon(playerChoice);
+        }
+        else {
+            play = new Dungeon();
+        }
+
+
+        String move;
+
+
+        while (true) {
+            do {
+                System.out.print("Choose Direction (n - North, e - East, s - South, w - West): ");
+                playerChoice = s.nextLine();
+                move = play.movePlayer(playerChoice);
+                System.out.println(move);
+            } while (move.equals("You walk into a wall"));
+            play.runRoom();
+        }
+    }
 }
