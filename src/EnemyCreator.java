6ee6b29d4ba6987b7d7db74587b0af8a64b70ab2
Christian Wan <christian.wan295@gmail.com>
2023-11-08 00:31:34 -0500

    Added on to the EnemyCreator class but can't figure out how to store a random attack into a new hashmap.

--- src/EnemyCreator.java
+++ src/EnemyCreator.java
@@ -1,18 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class EnemyCreator {
 
-    String name;
-    int healthPoints;
-    HashMap<String, Integer> attackMove = new HashMap<String, Integer>();
-    HashMap<String, Integer> defenceMove = new HashMap<String, Integer>();
-    HashMap<String, HashMap<String, Integer>> moveList = new HashMap<String, HashMap<String, Integer>>();
+    private String name;
+    private int healthPoints;
+    private HashMap<String, Integer> attackMove = new HashMap<String, Integer>();
+    private HashMap<String, Integer> defenseMove = new HashMap<String, Integer>();
+    private HashMap<String, HashMap<String, Integer>> moveList = new HashMap<String, HashMap<String, Integer>>();
+    private HashMap<String, Integer> currentAttack = new HashMap<String, Integer>();
 
     public EnemyCreator() {
-        File f = new File("Enemies/enemy" + Math.random());
+        File f = new File("enemies/enemy1");
         Scanner s = null;
         try {
             s = new Scanner(f);
@@ -33,30 +35,69 @@ public class EnemyCreator {
         options = line.split(", ");
         for (int i = 0; i < options.length; i++) {
             String[] singleMove = options[i].split(" ");
-            defenceMove.put(singleMove[0], Integer.parseInt(singleMove[1]));
+            defenseMove.put(singleMove[0], Integer.parseInt(singleMove[1]));
         }
-        moveList.put("Defense", defenceMove);
+        moveList.put("Defense", defenseMove);
     }
 
     public void changeHealth(int damage) {
+
         healthPoints -= damage;
     }
     public int getHealthPoints() {
+
         return healthPoints;
     }
-    public void attack() {
-        int randomAttack = (int) (Math.random() * 4) + 1;
+
+    public HashMap<String, Integer> getCurrentAttack() {
+        return currentAttack;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void getAttack() {
+        String temp = "";
+        int tempInt = 0;
+        HashMap<String, Integer> tempMap = new HashMap<String, Integer>();
+        int randomAttack = (int) (Math.random() * 3) + 1;
         if (randomAttack == 1) {
-
-        }
-        else if (randomAttack == 2) {
-
-        }
-        else if (randomAttack == 3) {
-
+            int random = (int) (Math.random() * defenseMove.size());
+            for (Map.Entry<String, Integer> entry : defenseMove.entrySet()) {
+                temp = entry.getKey();
+                tempInt  = entry.getValue();
+                random--;
+                if (random == 0) {
+                    break;
+                }
+            }
+            tempMap.put(temp, tempInt);
         }
         else {
+            int random = (int) (Math.random() * attackMove.size());
+            for (Map.Entry<String, Integer> entry : attackMove.entrySet()) {
+                temp = entry.getKey();
+                tempInt = entry.getValue();
+                random--;
+                if (random == 0) {
+                    break;
 
+                }
+            }
+            tempMap.put(temp, tempInt);
         }
+        currentAttack = tempMap;
+    }
+
+    public String enemyAttackPrint(HashMap<String, Integer> move) {
+        String attackName = "";
+        int damage = 0;
+        for (Map.Entry<String, Integer> entry : move.entrySet()) {
+            attackName = entry.getKey();
+            damage = entry.getValue();
+        }
+        String statement = "";
+
+        return "Enemy " + name + " " + attackName + " you for " + damage + " damage!";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,40 +6,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        HashMap<String, Integer> attackMove = new HashMap<String, Integer>();
-        HashMap<String, Integer> defenceMove = new HashMap<String, Integer>();
-        HashMap<String, HashMap<String, Integer>> moveList = new HashMap<String, HashMap<String, Integer>>();
+        EnemyCreator enemy = new EnemyCreator();
 
 
-
-        File f = new File("Enemies/enemy1");
-        Scanner s = null;
-        try {
-            s = new Scanner(f);
-        }
-        catch (FileNotFoundException fException) {
-            System.out.println("File not found.");
-        }
-        String name = s.nextLine();
-        int healthPoints = Integer.parseInt(s.nextLine());
-        String line = s.nextLine();
-        String[] options = line.split(", ");
-        for (int i = 0; i < options.length; i++) {
-            String[] singleMove = options[i].split(" ");
-            attackMove.put(singleMove[0], Integer.parseInt(singleMove[1]));
-        }
-        moveList.put("Attack", attackMove);
-        line = s.nextLine();
-        options = line.split(", ");
-        for (int i = 0; i < options.length; i++) {
-            String[] singleMove = options[i].split(" ");
-            defenceMove.put(singleMove[0], Integer.parseInt(singleMove[1]));
-        }
-        moveList.put("Defense", defenceMove);
-
-        System.out.println("Name: " + name);
-        System.out.println("Health: " + healthPoints);
-        System.out.println("Moves: " + moveList);
+        System.out.println("Health: " + enemy.getHealthPoints());
+        System.out.println(enemy.getName());
+        System.out.println(enemy.getCurrentAttack());
+        System.out.println(enemy.enemyAttackPrint(enemy.getCurrentAttack()));
 
         }
     }
