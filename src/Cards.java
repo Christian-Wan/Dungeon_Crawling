4463350abe38498005ed1dfc5ea39ca666603ef2
Christian-Wan <145377201+Christian-Wan@users.noreply.github.com>
2023-11-08 15:51:38 -0500

    Made constructor method for cards class

--- src/Cards.java
+++ src/Cards.java
@@ -1,10 +1,31 @@
-//public class Cards {
-//    public Cards() {}
-//
-//    public void punch(Object Enemy) {
-//        Enemy
-//    }
-//    public void kick() {}
-//
-//
-//}
+public class Cards {
+
+    private String type;
+
+    private int effectiveness;
+
+    private String status;
+
+    private String description;
+
+    
+
+
+
+    public Cards(String card) {
+        File f = new File("cards/" + card);
+        Scanner s = null;
+        try {
+            s = new Scanner(f);
+        }
+        catch (FileNotFoundException fException) {
+            System.out.println("File not found.");
+        }
+        this.name = s.nextLine();
+        this.healthPoints = Integer.parseInt(s.nextLine());
+        String line = s.nextLine();
+    }
+    
+
+
+}
